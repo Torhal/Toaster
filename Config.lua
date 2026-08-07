02351b331fd20a60efbc53eab39d7db352da03ff
James D. Callahan III <darkenelf@gmail.com>
2026-08-07 18:09:41 -0400

    DefaultOptions: Use CopyTable instead of copyTable since that apparently changed at some point.

--- Config.lua
+++ Config.lua
@@ -497,7 +497,7 @@ local function DefaultOptions()
                 type = "execute",
                 name = L["Reset Position"],
                 func = function()
-                    db.global.display.anchor = _G.copyTable(private.DATABASE_DEFAULTS.global.display.anchor)
+                    db.global.display.anchor = CopyTable(private.DATABASE_DEFAULTS.global.display.anchor)
                     LibWindow.RestorePosition(anchorFrame)
                 end,
             },
